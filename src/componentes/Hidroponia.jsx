e961dbca2ddc289944d15595b81ae2345b745c0e
emilianostic <emilianostic@gmail.com>
2024-04-18 22:47:23 -0300

    coloco el video en hidroponia

--- src/componentes/Hidroponia.jsx
+++ src/componentes/Hidroponia.jsx
@@ -1,7 +1,13 @@
 const Hidroponia = () =>{
 
     return (<>
-<h1>Hola</h1>
+<article>
+    <h4>Video del prototipo hidropónico NFT con hortalizas de hoja</h4>
+    <video width="250" height="200" controls autoplay onended="this.load()">
+        <source src="./video/VID-20220506-WA0025.mp4" type="video/mp4" />
+    </video>
+</article>
+
 </> );
 };
 export default Hidroponia
