b3c4716c2f24b5fc27bd0be1db3ce3e867556b93
emilianostic <emilianostic@gmail.com>
2024-05-05 22:11:35 -0300

    contacto con redes sociales, obligatoriedad nombre y correo, y estilo

--- src/componentes/Contacto.jsx
+++ src/componentes/Contacto.jsx
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import '../styles.css'; // Importamos un archivo CSS para los estilos
 
 const Contacto = () => {
+  const [nombreValido, setNombreValido] = useState(true);
+  const [emailValido, setEmailValido] = useState(true);
 
   const sendEmail = (event) => {
     event.preventDefault();
 
-    emailjs.sendForm('service_7c2gcp4','template_tzdmmf5',event.target,'NHQruOI3BzUISlenQ')
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
-  }
+    const nombre = event.target.user_name.value;
+    const email = event.target.user_email.value;
+
+    if (!nombre) {
+      setNombreValido(false);
+      return;
+    } else {
+      setNombreValido(true);
+    }
+
+    if (!email) {
+      setEmailValido(false);
+      return;
+    } else {
+      setEmailValido(true);
+    }
+
+    emailjs
+      .sendForm('service_7c2gcp4', 'template_tzdmmf5', event.target, 'NHQruOI3BzUISlenQ')
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className='flex flex-col items-center min-h-screen bg-gradient-to-br from-gray-900 to-gray-600'>
-      <h1 className='text-orange-500 text-5xl font-semibold font-shadows'>Contacto (Contact Me)</h1>
-      <form className='w-80 border-4 border-white rounded-lg shadow-lg bg-gradient-to-br from-gray-800 to-gray-600 bg-opacity-80 p-4 flex flex-col items-center' onSubmit={sendEmail}>
-        <label className='text-blue-300 text-xl font-bold mb-2'>Nombre (Name)</label>
-        <input type="text" name='user_name' className='w-full text-white bg-transparent rounded-full px-4 py-2 mb-4' />
+      <h1 className='text-white text-xl font-semibold font-shadows'>Contactémonos</h1>
+      <form className='w-100 border-4 border-white rounded-lg shadow-lg bg-gray-800 text-white bg-opacity-80 p-4 flex flex-col items-center' onSubmit={sendEmail}>
+        <label className='text-white text-xl font-bold mb-2'>Nombre</label>
+        <input type="text" name='user_name' className={`w-full text-white bg-transparent rounded-full px-4 py-2 mb-4 ${nombreValido ? '' : 'border-red-500'}`} placeholder='Juan Pérez'/>
+        {!nombreValido && <span className="text-red-500 text-sm">Debés ingresar tu nombre</span>}
         <hr className='w-full' />
 
-        <label className='text-blue-300 text-xl font-bold mb-2'>Correo electrónico (Email)</label>
-        <input type="email" name='user_email' className='w-full text-white bg-transparent rounded-full px-4 py-2 mb-4' />
+        <label className='text-white text-xl font-bold mb-2'>Correo electrónico</label>
+        <input type="email" name='user_email' className={`w-full text-white bg-transparent rounded-full px-4 py-2 mb-4 ${emailValido ? '' : 'border-red-500'}`} placeholder='usuario@email.com'/>
+        {!emailValido && <span className="text-red-500 text-sm">Debes ingresar un e-mail válido</span>}
         <hr className='w-full' />
 
-        <label className='text-blue-300 text-xl font-bold mb-2'>Mensaje (Message)</label>
-        <textarea name="user_message" id="" cols="30" rows="10" className='w-full bg-gray-400 rounded-lg px-4 py-2 mb-4'></textarea>
+        <label className='text-white text-xl font-bold mb-2'>Instagram</label>
+        <input type="text" name='instagram' className='w-full text-white bg-transparent rounded-full px-4 py-2 mb-4' placeholder='@usuario'/>
         <hr className='w-full' />
-        <button className='w-32 rounded-full text-xl text-green-500 bg-green-300 hover:bg-green-600 hover:text-white transition duration-300'>Enviar (Send)</button>
+
+        <label className='text-white text-xl font-bold mb-2'>LinkedIn</label>
+        <input type="text" name='linkedin' className='w-full text-white bg-transparent rounded-full px-4 py-2 mb-4' placeholder='@usuario'/>
+        <hr className='w-full' />
+
+        <label className='text-white text-xl font-bold mb-2'>Mensaje</label>
+        <textarea name="user_message" cols="30" rows="4" className='w-full bg-gray-400 rounded-lg px-4 py-2 mb-4' placeholder='...'></textarea>
+        <hr className='w-full' />
+
+        
+        <button className='w-32 rounded-full text-xl text-black bg-gray-300 hover:bg-gray-400 hover:text-gray-800 transition duration-300'>Enviar</button>
       </form>
     </div>
-  )
-}
-export default Contacto
+  );
+};
+
+export default Contacto;
+
 
