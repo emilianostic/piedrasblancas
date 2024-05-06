103a37dcf9d43656396bfa0886b4f49e73672367
emilianostic <emilianostic@gmail.com>
2024-05-06 16:40:07 -0300

    cambio a tailwind en hidroponia

--- src/componentes/Hidroponia.jsx
+++ src/componentes/Hidroponia.jsx
@@ -1,29 +1,26 @@
-import '../styles.css'; // Importamos un archivo CSS para los estilos
-import evhidroponia from '../img/EV-hidroponia-round.png' // Importa tu imagen aquí
-import video from '../video/videoHidrop.mp4'
-const Hidroponia = () =>{
+import evhidroponia from '../img/EV-hidroponia-round.png'; // Importa tu imagen aquí
+import video from '../video/videoHidrop.mp4';
 
-    return (<>
-    <img src={evhidroponia} alt="ev hidroponia" className="evHidroponia" />
+const Hidroponia = () => {
+  return (
+    <>
+      <img src={evhidroponia} alt="ev hidroponia" className="mx-auto w-20" />
 
-    <article className='hidroponia'>
-    ¿Qué es la Hidroponia?
-    La hidroponía es una técnica de cultivo en la que no se utiliza suelo, y los nutrientes
-    que necesita la planta para crecer son provistos a través del agua.
-    </article>
+      <article className='text-center mt-2'>
+        En EV hidroponía se cultivan hortalizas de hoja como kale, rúcula, lechuga y col asiática. Además realizamos el cultivo de albahaca. La técnica utilizada es la N.F.T. o de la lámina de nutrientes (del inglés N.F.T. -Nutrient Film Technique-) en la cuál se utiliza una solución que está en un contenedor con una bomba que permite que la misma circule por los canales que sostienen a las plantas haciendo que la lámina con los nutrientes vaya tomando contacto con las raíces para luego ser recolectada para volver al recipiente y ser bombeada nuevamente.
+      </article>
 
-    <div className="flex flex-col items-center justify-center h-full">
-    <h4 className="mb-4">Video del prototipo hidropónico NFT (Nutrient Film Technique) con hortalizas de hoja.</h4>
-    
-    <div className="w-full md:w-auto">
-        <video className="w-full h-auto md:h-auto" controls>
+      <div className="flex flex-col items-center justify-center h-full">
+        <h4 className="mb-4">Video del prototipo hidropónico NFT (Nutrient Film Technique) con hortalizas de hoja.</h4>
+
+        <div className="w-full md:w-auto">
+          <video className="w-full h-auto md:h-auto" controls>
             <source src={video} type="video/mp4" />
-        </video>
-    </div>
-</div>
-
-
-
-</> );
+          </video>
+        </div>
+      </div>
+    </>
+  );
 };
-export default Hidroponia
+
+export default Hidroponia;
--- src/componentes/YesoConstruccion.jsx
+++ src/componentes/YesoConstruccion.jsx
@@ -1,32 +1,35 @@
 import '../styles.css'; // Importamos un archivo CSS para los estilos
-import YesoerPlaK from '../img/YesoerPlaK.png' // Importa tu imagen aquí
+import YesoerPlaK from '../img/YesoerPlaK.png'; // Importa tu imagen aquí
 
-const YesoConstruccion = () =>{
-
-    return (<>
-
-<img src={YesoerPlaK} alt="YesoerPlaK" className="yesoerPlaK" />
-<article>
-Denominación Comercial:
-YESOER PLAK
-
-Descripción Técnica:
-Yeso calcinado tipo hemihidratado de alta pureza, alta cremosidad, color blanco.
-Aplicaciones del Producto
-Materiales prefabricados y todo tipo de moldería de alta resistencia. Ortopedia.
-
-Embalaje de presentación comercial:
-	Bolsas de 40 Kg
-	Big Bags
-
-    </article>
-    <article>
-        Yeso Tradicional
-    </article>
-    <article>
-        Yeso Obra Manual
-    </article>
-
-</> );
+const YesoConstruccion = () => {
+  return (
+    <>
+      <img
+        src={YesoerPlaK}
+        alt="YesoerPlaK"
+        className="text-center w-20 mx-auto"
+      />
+      <article className="text-center">
+        <p>Denominación Comercial: YESOER PLAK</p>
+        <p>
+          Descripción Técnica: Yeso calcinado tipo hemihidratado de alta
+          pureza, alta cremosidad, color blanco.
+        </p>
+        <p>Aplicaciones del Producto: Materiales prefabricados y todo tipo de moldería de alta resistencia. Ortopedia.</p>
+        <p>Embalaje de presentación comercial:</p>
+        <ul>
+          <li>Bolsas de 40 Kg</li>
+          <li>Big Bags</li>
+        </ul>
+      </article>
+      <article className="text-center">
+        <p>Yeso Tradicional</p>
+      </article>
+      <article className="text-center">
+        <p>Yeso Obra Manual</p>
+      </article>
+    </>
+  );
 };
-export default YesoConstruccion
+
+export default YesoConstruccion;
